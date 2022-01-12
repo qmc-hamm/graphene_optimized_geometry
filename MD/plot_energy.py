0f17cf0854d05ddaf3bd60c51bf5ab23e41591d4
krongch2 <krongch2@illinois.edu>
2022-01-12 15:37:48 -0600

    back up before side by side

--- MD/plot_energy.py
+++ MD/plot_energy.py
@@ -31,33 +31,42 @@ def dumpfile_to_df(dumpfile):
 
     return df, latvec
 
-def scatterplotter(x, y, z, title, colorbar_label, output, strip_width=None, vmin=None, vmax=None):
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3, 5))
-    p = plt.scatter(x, y, c=z, cmap='jet', s=2, alpha=0.8, lw=0)
+def get_data(twist_angle, potential, atom_type=1):
+    d_all, latvec = dumpfile_to_df(f"kc_rebo_{potential}/raw/simulations/{twist_angle}/dump_final.txt")
 
-    cax = fig.add_axes([
-        ax.get_position().x1+0.01 + 0.1,
-        ax.get_position().y0 + 0.05,
-        0.02,
-        ax.get_position().height
-        ])
-    cbar = plt.colorbar(p, cax=cax)
-    cbar.set_label(colorbar_label, rotation=90, fontsize=9, fontweight='bold', labelpad=8)
-    cbar.ax.tick_params(labelsize=9)
+    # select only one layer, e.g. for the bottom layer, `atom_type` is 1
+    d = d_all.loc[d_all.atom_type == atom_type, :].reset_index(0, drop=True)
+
+    # divide the angle by two because the geometry is setup such that
+    # the top layer is twisted counter-clockwise by `twist_angle`/2,
+    # and the bottom layer is twisted clockwise by `twist_angle`/2
+    theta = float(twist_angle.replace('-', '.'))/2 *np.pi/180
+    d_rotated = d.copy()
+    d_rotated[['x', 'y', 'z']] = rotate_coords(d[['x', 'y', 'z']], theta)
+    d_rotated['potential'] = potential
+
+    # fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3, 5))
+    # ax = scatterplotter(ax, d_rotated.x, d_rotated.y, d_rotated.energy, label, strip_width=strip_width, vmin=-7.427, vmax=-7.412)
+    # plt.savefig(f'{twist_angle}_{potential}_energy.pdf')
+    # plt.close()
+    return d_rotated
+
+def scatterplotter(ax, x, y, z, title, strip_width=None, vmin=None, vmax=None):
+    p = ax.scatter(x, y, c=z, cmap='jet', s=2, alpha=0.8, lw=0)
     ax.text(0.1, 0.9, title, transform=ax.transAxes)
     if strip_width is not None:
         ax.axvspan(0, strip_width, alpha=0.5, color='#2f2f2f')
     vmin = vmin if vmin is not None else np.min(z)
     vmax = vmax if vmax is not None else np.max(z)
-    plt.clim(vmin=vmin, vmax=vmax)
-    plt.xticks(fontsize=9)
-    plt.yticks(fontsize=9)
+    # plt.clim(vmin=vmin, vmax=vmax)
+    # ax.set_xticks(fontsize=9)
+    # ax.set_yticks(fontsize=9)
 
     ax.set_aspect('equal', 'box')
     ax.set(xlabel='$x$ (ang)', ylabel='$y$ (ang)')
-    fig.tight_layout()
-    plt.savefig(output, bbox_inches='tight')
-    plt.close()
+    # fig.tight_layout()
+    # plt.savefig(output, bbox_inches='tight')
+    return ax, p
 
 # def lineplotter(x, y_ouyang, y_refit, xlabel, ylabel, output, plot=True):
 #     x_ouyang1, x_ouyang2 = find_sp_peaks(x, y_ouyang)
@@ -96,23 +105,26 @@ def rotate_coords(coords, theta):
     rotated = rotation.dot(coords.T).T
     return rotated
 
-def plot_energy(twist_angle, potential, label, atom_type=1, strip_width=None):
-    d_all, latvec = dumpfile_to_df(f"kc_rebo_{potential}/raw/simulations/{twist_angle}/dump_final.txt")
+def plot_energy_side_by_side(twist_angle, pot1, pot2, label1, label2, atom_type=1, strip_width=None):
+    d1 = get_data(twist_angle, pot1, strip_width=strip_width)
+    d2 = get_data(twist_angle, pot2, strip_width=strip_width)
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(7, 5))
 
-    # select only one layer, e.g. for the bottom layer, `atom_type` is 1
-    d = d_all.loc[d_all.atom_type == atom_type, :].reset_index(0, drop=True)
+    cax = fig.add_axes([
+        ax1.get_position().x1 + 0.01 + 0.1,
+        ax1.get_position().y0 + 0.05,
+        0.02,
+        ax1.get_position().height
+        ])
+    p, ax1 = scatterplotter(ax1, d1.x, d1.y, d1.energy, label1, strip_width=strip_width, vmin=-7.427, vmax=-7.412)
+    p, ax2 = scatterplotter(ax2, d2.x, d2.y, d2.energy, label2, strip_width=strip_width, vmin=-7.427, vmax=-7.412)
+    cbar = fig.colorbar(p, cax=cax)
+    # cbar = plt.colorbar(p, cax=cax)
 
-    # divide the angle by two because the geometry is setup such that
-    # the top layer is twisted counter-clockwise by `twist_angle`/2,
-    # and the bottom layer is twisted clockwise by `twist_angle`/2
-    theta = float(twist_angle.replace('-', '.'))/2 *np.pi/180
-    d_rotated = d.copy()
-    d_rotated[['x', 'y', 'z']] = rotate_coords(d[['x', 'y', 'z']], theta)
-    d_rotated['potential'] = potential
-
-    scatterplotter(d_rotated.x, d_rotated.y, d_rotated.energy, label, 'Energy (eV/atom)', f'{twist_angle}_{potential}_energy.pdf',
-        strip_width=strip_width, vmin=-7.427, vmax=-7.412)
-    return d_rotated
+    # cbar.set_label('Energy (eV/atom)', rotation=90, fontsize=9, fontweight='bold', labelpad=8)
+    # cbar.ax.tick_params(labelsize=9)
+    # p.set_clim(vmin=vmin, vmax=vmax)
+    plt.savefig(f'{twist_angle}_side_by_side.pdf')
 
 def plot_energy_strip(d, output):
     g = sns.FacetGrid(data=d, hue='potential')
@@ -132,9 +144,10 @@ if __name__ == '__main__':
     widths = []
     for twist_angle in ['0-99']:
         strip_width = 0.25
-        d_ouyang = plot_energy(twist_angle, 'ouyang', '(a) Ouyang', strip_width=strip_width)
-        d_refit = plot_energy(twist_angle, 'refit', '(b) Refit', strip_width=strip_width)
-        d = pd.concat([d_ouyang, d_refit], ignore_index=True)
-        d_strip = d.loc[(-strip_width <= d.x) & (d.x < strip_width), :]
-        print(d_strip.shape)
-        plot_energy_strip(d_strip, f'{twist_angle}_energy_strip.pdf')
+        # d_ouyang = plot_energy(twist_angle, 'ouyang', '(a) Ouyang', strip_width=strip_width)
+        # d_refit = plot_energy(twist_angle, 'refit', '(b) QMC', strip_width=strip_width)
+        plot_energy_side_by_side(twist_angle, 'ouyang', 'refit', '(a) Ouyang', '(b) QMC', atom_type=1, strip_width=None)
+        # d = pd.concat([d_ouyang, d_refit], ignore_index=True)
+        # d_strip = d.loc[(-strip_width <= d.x) & (d.x < strip_width), :]
+        # print(d_strip.shape)
+        # plot_energy_strip(d_strip, f'{twist_angle}_energy_strip.pdf')
