94c77dae38ca7ed9e1ff400f666292f8358c58bb
krongch2 <krongch2@illinois.edu>
2022-02-01 10:03:20 -0600

    add plot_z_side_by_side

--- MD/plot_energy.py
+++ MD/plot_energy.py
@@ -77,8 +77,8 @@ def get_disp_data(twist_angle, pot):
     d_init['mag'] = (d_init['dx']**2 + d_init['dy']**2)**0.5
     return d_init
 
-def scatterplotter(fig, ax, x, y, z, title, ylabel=None, colorbar=True, strip_width=None):
-    p = ax.scatter(x, y, c=z, cmap='jet', s=2, lw=0, vmin=-7.427, vmax=-7.412)
+def scatterplotter(fig, ax, x, y, z, title, vmin=-7.427, vmax=-7.412, ylabel=None, colorbar=True, strip_width=None):
+    p = ax.scatter(x, y, c=z, cmap='jet', s=2, lw=0, vmin=vmin, vmax=vmax)
     ax.text(0.1, 1.02, title, transform=ax.transAxes)
     if strip_width is not None:
         ax.axvspan(0, strip_width, alpha=0.5, color='#2f2f2f')
@@ -115,27 +115,24 @@ def quiverplotter(fig, ax, x, y, dx, dy, mag, title, ylabel=None, colorbar=True)
             ])
         cax.set_title('Energy (eV/atom)')
         ax.figure.colorbar(p, cax=cax)
-    # cbar = plt.colorbar()
-    # cbar.set_label('In-plane displacement ($\AA$)',rotation=90, fontsize = 20, fontweight = 'bold', labelpad=20)
-    # cbar.ax.tick_params(labelsize = 18)
-    # plt.xticks(fontsize = 18)
-    # plt.yticks(fontsize = 18)
-    # fig = plt.gcf()
-    # fig.set_size_inches(7.5, 10)
-    # plt.tight_layout()
-    # fig.savefig(figname, dpi=200)
-    # plt.show()
-
 
 def plot_energy_side_by_side(twist_angle, pot1, pot2, label1, label2, atom_type=1):
     d1 = get_data(twist_angle, pot1)
     d2 = get_data(twist_angle, pot2)
     fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(6, 5))
-    scatterplotter(fig, ax1, d1.x, d1.y, d1.energy, label1, ylabel='$y$ (ang)', colorbar=False)
-    scatterplotter(fig, ax2, d2.x, d2.y, d2.energy, label2, ylabel=None, colorbar=True)
+    scatterplotter(fig, ax1, d1.x, d1.y, d1.energy, label1, vmin=-7.427, vmax=-7.412, ylabel='$y$ (ang)', colorbar=False)
+    scatterplotter(fig, ax2, d2.x, d2.y, d2.energy, label2, vmin=-7.427, vmax=-7.412, ylabel=None, colorbar=True)
     fig.tight_layout()
     plt.savefig(f'{twist_angle}_energy.pdf', bbox_inches='tight')
 
+def plot_z_side_by_side(twist_angle, pot1, pot2, label1, label2, atom_type=1):
+    d1 = get_data(twist_angle, pot1)
+    d2 = get_data(twist_angle, pot2)
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(6, 5))
+    scatterplotter(fig, ax1, d1.x, d1.y, d1.z, label1, vmin=2.9, vmax=3.1, ylabel='$y$ (ang)', colorbar=False)
+    scatterplotter(fig, ax2, d2.x, d2.y, d2.z, label2, vmin=2.9, vmax=3.1, ylabel=None, colorbar=True)
+    fig.tight_layout()
+    plt.savefig(f'{twist_angle}_z.pdf', bbox_inches='tight')
 
 def plot_displacement_side_by_side(twist_angle, pot1, pot2, label1, label2, atom_type=1):
     d1 = get_disp_data(twist_angle, pot1)
@@ -166,8 +163,10 @@ if __name__ == '__main__':
         strip_width = 0.25
         # d_ouyang = plot_energy(twist_angle, 'ouyang', '(a) Ouyang', strip_width=strip_width)
         # d_refit = plot_energy(twist_angle, 'refit', '(b) QMC', strip_width=strip_width)
-        # plot_energy_side_by_side(twist_angle, 'ouyang', 'refit', '(a) Ouyang', '(b) QMC', atom_type=1)
+        plot_energy_side_by_side(twist_angle, 'ouyang', 'refit', '(a) Ouyang', '(b) QMC', atom_type=1)
+        plot_z_side_by_side(twist_angle, 'ouyang', 'refit', '(a) Ouyang', '(b) QMC', atom_type=1)
         plot_displacement_side_by_side(twist_angle, 'ouyang', 'refit', '(a) Ouyang', '(b) QMC', atom_type=1)
+
         # d = pd.concat([d_ouyang, d_refit], ignore_index=True)
         # d_strip = d.loc[(-strip_width <= d.x) & (d.x < strip_width), :]
         # print(d_strip.shape)
